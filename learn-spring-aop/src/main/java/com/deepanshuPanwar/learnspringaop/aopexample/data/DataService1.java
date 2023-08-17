13d0359451c35ab556d9a276384962b07b0a5798
Deepanshu Panwar <deepanshu2893@gmail.com>
2023-08-17 23:20:49 +0530

    Update

---
+++ learn-spring-aop/src/main/java/com/deepanshuPanwar/learnspringaop/aopexample/data/DataService1.java
@@ -0,0 +1,9 @@
+package com.deepanshuPanwar.learnspringaop.aopexample.data;
+
+public class DataService1 {
+
+    public int[] retrievingData(){
+        return new int [] {11, 22, 33, 44, 55};
+
+    }
+}
