eb32abaf939849390dbea4139fbf7fc6f563ff9b
Deepanshu Panwar <110801678+deepanshu2893@users.noreply.github.com>
2023-08-17 23:24:03 +0530

    Delete learn-spring-aop directory

--- learn-spring-aop/src/main/java/com/deepanshuPanwar/learnspringaop/aopexample/data/DataService1.java
+++
@@ -1,9 +0,0 @@
-package com.deepanshuPanwar.learnspringaop.aopexample.data;
-
-public class DataService1 {
-
-    public int[] retrievingData(){
-        return new int [] {11, 22, 33, 44, 55};
-
-    }
-}
