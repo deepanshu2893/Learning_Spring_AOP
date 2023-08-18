ce14ed636cd4c20d31752cb54a5aafdda30b1c25
Deepanshu Panwar <deepanshu2893@gmail.com>
2023-08-18 21:12:18 +0530

    Using maven since gradle was giving error

---
+++ learning-spring-aop/src/main/java/com/DeepanshuPanwar/learningspringaop/LearningSpringAopApplication.java
@@ -0,0 +1,13 @@
+package com.DeepanshuPanwar.learningspringaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringAopApplication.class, args);
+	}
+
+}
---
+++ learning-spring-aop/src/test/java/com/DeepanshuPanwar/learningspringaop/LearningSpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DeepanshuPanwar.learningspringaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
